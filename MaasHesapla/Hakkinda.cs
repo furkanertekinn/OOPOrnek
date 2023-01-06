efb448304c579d4eeb86efea9a5fb3c6eeb6ae9f
Furkan Ertekin <furkanertekin0@gmail.com>
2023-01-06 22:26:49 +0300

    first commit

---
+++ MaasHesapla/Hakkinda.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornekleme
+{
+    class Hakkinda
+    {
+        public Hakkinda(double _sene1)
+        {
+            double artis1 = (_sene1 * 30) / 100;
+            double toplamYilBoyuAldigiMaas1 = (_sene1 + artis1) * 12;
+
+            double toplam = toplamYilBoyuAldigiMaas1;
+            SonucGösterme(toplam);
+        }
+
+        public Hakkinda(double _sene1, double _sene2)
+        {
+            double artis1 = (_sene1 * 30) / 100;
+            double toplamYilBoyuAldigiMaas1 = (_sene1 + artis1) * 12;
+
+            double artis2 = (_sene2 * 40) / 100;
+            double toplamYilBoyuAldigiMaas2 = (_sene2 + artis2) * 12;
+
+            double toplam = toplamYilBoyuAldigiMaas1 + toplamYilBoyuAldigiMaas2;
+            SonucGösterme(toplam);
+        }
+
+        public Hakkinda(double _sene1, double _sene2, double _sene3)
+        {
+            double artis1 = (_sene1 * 30) / 100;
+            double toplamYilBoyuAldigiMaas1 = (_sene1 + artis1) * 12;
+
+            double artis2 = (_sene2 * 40) / 100;
+            double toplamYilBoyuAldigiMaas2 = (_sene2 + artis2) * 12;
+
+            double artis3 = (_sene3 * 50) / 100;
+            double toplamYilBoyuAldigiMaas3 = (_sene3 + artis3) * 12;
+
+            double toplam = toplamYilBoyuAldigiMaas1 + toplamYilBoyuAldigiMaas2 + toplamYilBoyuAldigiMaas3;
+            SonucGösterme(toplam);
+        }
+
+        public void SonucGösterme(double toplamYilBoyuAldigiMaas)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Sonuç : "+toplamYilBoyuAldigiMaas);
+
+            if (toplamYilBoyuAldigiMaas<50000)
+            {
+                Console.WriteLine("Düşük gelirlisiniz.");
+            }
+
+            else if (toplamYilBoyuAldigiMaas>=50000||toplamYilBoyuAldigiMaas<100000)
+            {
+                Console.WriteLine("Orta gelirlisiniz.");
+            }
+
+            else
+            {
+                Console.WriteLine("Yüksek gelirilsiniz.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MaasHesapla/main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornekleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double girilenSene;
+
+            while (true)
+            {
+                Console.Write("Kaç senelik maaşınızı girmek istiyorsunuz? : ");
+                girilenSene = Convert.ToDouble(Console.ReadLine());
+
+                if (Convert.ToInt32(girilenSene) > 0 && Convert.ToInt32(girilenSene) < 4)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen belirtilen koşula göre giriş yapınız!");
+                }
+            }
+
+            double[] dizi = new double[Convert.ToInt32(girilenSene)];
+
+            while (true)
+            {
+                for (int i = 0; i < girilenSene; i++)
+                {
+                    Console.Write("{0}. seneki maaşınızı giriniz : ", i + 1);
+                    dizi[i] = Convert.ToDouble(Console.ReadLine());
+                }
+                break;
+            }
+
+            if (Convert.ToInt32(girilenSene) == 1)
+            {
+                Hakkinda H1 = new Hakkinda(dizi[0]);
+            }
+
+            else if (Convert.ToInt32(girilenSene) == 2)
+            {
+                Hakkinda H1 = new Hakkinda(dizi[0], dizi[1]);
+            }
+
+            else if (Convert.ToInt32(girilenSene) == 3)
+            {
+                Hakkinda H1 = new Hakkinda(dizi[0], dizi[1], dizi[2]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
