2c054201da8c90a878ddbe1a7a61cb6c95bacf1c
Furkan Ertekin <furkanertekin0@gmail.com>
2023-01-08 18:02:02 +0300

    first commit

---
+++ KullaniciBilgi/Musteri.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsulleme2
+{
+    internal class Musteri
+    {
+        int id;
+        string emailAdres;
+     
+        public string EmailAdres
+        {
+            get
+            {
+                return this.emailAdres;
+            }
+            set
+            {
+                this.emailAdres = value;
+            }
+        }
+
+        public Musteri()
+        {
+            this.id = IDuret();
+        }
+        public int ID
+        {
+            get
+            {
+                return this.id;
+            }
+
+            private set
+            {
+                this.id = value;
+            }
+
+        }
+
+        public int IDuret()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1000, 100000);
+        }
+
+
+
+
+    }
+}
---
+++ KullaniciBilgi/main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsulleme2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1= new Musteri();
+
+            M1.EmailAdres = "furkan.ertekin@hotmail.com";
+
+            Console.WriteLine("Kullanıcı Bilgileri ");
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Email adres : "+M1.EmailAdres);
+            Console.WriteLine(" ");
+            Console.WriteLine("ID numarası : "+M1.ID.ToString());
+            Console.ReadLine();
+        }
+    }
+}
