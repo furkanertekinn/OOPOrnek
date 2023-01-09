69db7a89256c92cef4c16707ea9d10afcd0b1b9c
Furkan Ertekin <furkanertekin0@gmail.com>
2023-01-09 11:32:33 +0300

    first commit

---
+++ TCkimlik/TC.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeOdev
+{
+    internal class TC
+    {
+        private string tckimliknumarasi;
+
+        public string TcKimlikNumarasi
+        {
+            get
+            {
+                Console.WriteLine(" ");
+                return tckimliknumarasi.Substring(0, 3);
+            }
+
+            set
+            {
+                if (value.Length == 11)
+                {
+                    bool kontrol = false;
+
+                    for (int i = 0; i < value.Length - 1; i++)
+                    {
+                        bool karakterKontrol = char.IsNumber(value[i]);
+
+                        if (karakterKontrol)
+                        {
+
+                        }
+
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+
+                    if (kontrol)
+                    {
+                        Console.WriteLine(" ");
+                        Console.WriteLine("TC kimlik numarası içindeki değerler sayısal olmalıdır!");
+                    }
+
+                    else
+                    {
+                        this.tckimliknumarasi = value;
+                    }
+
+                }
+
+                else
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("TC kimlik numaranız 11 haneli olmalıdır!");
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ TCkimlik/main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TC T1 = new TC();
+
+            Console.Write("Lütfen TC kimlik numaranızı giriniz : ");
+
+            T1.TcKimlikNumarasi = Console.ReadLine();
+
+            string TC = T1.TcKimlikNumarasi;
+
+            Console.WriteLine("TC kimlik numaranızın ilk '3' hanesi : " + TC);
+
+            Console.ReadLine();
+        }
+    }
+}
