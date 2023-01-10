5e0854a442bd19aa55eae7fb45a267f1b7dffd86
Furkan Ertekin <furkanertekin0@gmail.com>
2023-01-10 23:16:22 +0300

    first commit

---
+++ GenelGirisBilgi/Musteri.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticOdev
+{
+    public class Musteri
+    {
+        static ArrayList MusteriDataBase;
+        static Musteri()
+        {
+            MusteriDataBase = new ArrayList();
+        }
+
+        public int MusteriID { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string emailadres { get; set; }
+        public string Sifre { get; set; }
+
+
+        public string _kullaniciAdi;
+        public string KullaniciAdi
+        {
+            get { return this._kullaniciAdi; }
+
+            set
+            {
+                bool kullaniciAdiKontrol = musteriKullaniciAdiKontrol(value);
+                if (kullaniciAdiKontrol)
+                {
+                    Console.WriteLine("Eklemek istediğiniz kullanıcı adı sistem içerisinde kayıtlı.");
+                    this._kullaniciAdi = string.Empty;
+                }
+                else
+                {
+                    this._kullaniciAdi = value;
+                }
+            }
+        }
+
+        static bool musteriKullaniciAdiKontrol(string _kullaniciAdi)
+        {
+            bool Kontrol = false;
+
+            for (int i = 0; i < MusteriDataBase.Count; i++)
+            {
+                Musteri Temp = (Musteri)MusteriDataBase[i];
+
+                if (Temp.KullaniciAdi == _kullaniciAdi)
+                {
+                    Kontrol = true;
+                    break;
+                }
+            }
+            return Kontrol;
+        }
+
+        public static void MusteriEkle(Musteri M)
+        {
+            if (M != null && !string.IsNullOrEmpty(M.KullaniciAdi)&& !string.IsNullOrEmpty(M.emailadres))
+            {
+                bool emailAdresKontrol = musteriEmailAdresKontrol(M.emailadres);
+
+                if (emailAdresKontrol)
+                {
+                    Console.WriteLine("Eklemek istediğiniz kullanıcı sistemde kayıtlı.");
+                }
+                else
+                {
+                    MusteriDataBase.Add(M);
+                    Console.WriteLine("Yeni kayıt işlemi başarılı");
+                }
+            }
+        }
+
+        static bool musteriEmailAdresKontrol(string _emailAdres)
+        {
+            bool Kontrol = false;
+            for (int i = 0; i < MusteriDataBase.Count; i++)
+            {
+                Musteri Temp = (Musteri)MusteriDataBase[i];
+                if (Temp.emailadres == _emailAdres)
+                {
+                    Kontrol = true;
+                    break;
+                }
+            }
+            return Kontrol;
+        }
+
+    }
+}
---
+++ GenelGirisBilgi/main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1= new Musteri();
+
+            M1.MusteriID= 111;
+            M1.isim = "Furkan";
+            M1.soyisim = "Ertekin";
+            M1.emailadres = "furkan.ertekin@hotmail.com";
+            M1.KullaniciAdi = "furkan.ertekin.123";
+            M1.Sifre = "456";
+
+            Musteri.MusteriEkle(M1);
+
+            Musteri M2 = new Musteri()
+            {
+                MusteriID = 222,
+                isim = "Osman",
+                soyisim = "Ertekin",
+                emailadres = "osman.ertekin@hotmail.com",
+                KullaniciAdi="osman.ertekin.456",
+                Sifre="1230"
+            };
+
+            Musteri.MusteriEkle(M2);
+
+            Musteri M3 = new Musteri();
+
+            M3.MusteriID = 111;
+            M3.isim = "Furkan";
+            M3.soyisim = "Ertekin";
+            M3.emailadres = "furkan.ertekin@hotmail.com";
+            M3.KullaniciAdi = "furkan.ertekin.123";
+            M3.Sifre = "456";
+
+            Musteri.MusteriEkle(M3);
+
+
+        }
+    }
+}
