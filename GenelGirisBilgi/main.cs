e5ad9c9983fc513423507900c3c8bdd7241f5e7e
Furkan Ertekin <furkanertekin0@gmail.com>
2023-01-15 15:21:42 +0300

    guncelleme

--- GenelGirisBilgi/main.cs
+++ GenelGirisBilgi/main.cs
@@ -10,14 +10,30 @@ namespace StaticOdev
     {
         static void Main(string[] args)
         {
-            Musteri M1= new Musteri();
+            Musteri M1 = new Musteri();
 
-            M1.MusteriID= 111;
-            M1.isim = "Furkan";
-            M1.soyisim = "Ertekin";
-            M1.emailadres = "furkan.ertekin@hotmail.com";
-            M1.KullaniciAdi = "furkan.ertekin.123";
-            M1.Sifre = "456";
+            Console.Write("Lütfen müşteri ID numaranızı giriniz : ");
+            M1.MusteriID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            Console.Write("Lütfen isminizi giriniz : ");
+            M1.isim = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            Console.Write("Lütfen soyadınızı giriniz : ");
+            M1.soyisim = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            Console.Write("Lütfen email adresinizi giriniz : ");
+            M1.emailadres = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            Console.Write("Lütfen kullanıcı adınızı giriniz : ");
+            M1.KullaniciAdi = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            Console.Write("Lütfen şifrenizi giriniz : ");
+            M1.Sifre = Console.ReadLine();
 
             Musteri.MusteriEkle(M1);
 
@@ -27,8 +43,8 @@ namespace StaticOdev
                 isim = "Osman",
                 soyisim = "Ertekin",
                 emailadres = "osman.ertekin@hotmail.com",
-                KullaniciAdi="osman.ertekin.456",
-                Sifre="1230"
+                KullaniciAdi = "osman.ertekin.456",
+                Sifre = "1230"
             };
 
             Musteri.MusteriEkle(M2);
