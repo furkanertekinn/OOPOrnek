611cb26acbc7969474dc70f6f56ba7e8ba25c799
Furkan Ertekin <furkanertekin0@gmail.com>
2023-01-27 22:52:06 +0300

    first commit

---
+++ Donustur/Donustur.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donustur
+{
+    public class Donustur
+    {
+        public int[] temelDizi;
+        public int[] yeniDizi;
+
+        public Donustur()
+        {
+
+        }
+        public Donustur(string veri)
+        {
+            temelDizi = new int[veri.Length];
+            yeniDizi = new int[veri.Length];
+
+            Console.Write("{");
+
+            for (int i = 0; i < veri.Length; i++)
+            {
+                Console.Write(veri[i]);
+                if (i<veri.Length-1)
+                {
+                    Console.Write(",");
+                    Console.Write(" ");
+                }
+                temelDizi[i] = (int)Char.GetNumericValue(veri[i]);
+            }
+
+            Console.Write("}");
+            
+        }
+
+        public void Ayarla(bool yon)
+        {
+            
+            yon = true;
+
+            if (yon)
+            {
+                Console.Write("{");
+                temelDizi[7] = yeniDizi[7];
+                Console.Write(yeniDizi[7]);
+
+                for (int i = 7; i <= yeniDizi.Length; i--)
+                {
+                    int cikartma = temelDizi[i - 1] - temelDizi[i];
+                    Console.Write(cikartma);
+                    Console.Write(",");
+                }
+                Console.WriteLine("}");
+            }
+
+            else if (!yon)
+            {
+                Console.Write("{");
+                temelDizi[0] = yeniDizi[0];
+                Console.Write(yeniDizi[0]);
+
+                for (int i = 0; i < yeniDizi.Length; i++)
+                {
+                    int cikartma = temelDizi[i + 1] - temelDizi[i];
+                    Console.Write(cikartma);
+                    Console.Write(",");
+                }
+                Console.WriteLine("}");
+            }
+
+            else
+            {
+                Console.WriteLine("Geçersiz giriş!");
+            }
+        }
+    }
+}
---
+++ Donustur/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donustur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir veri giriniz : ");
+            string kullaniciVeri=Console.ReadLine();
+
+            Donustur D1 = new Donustur(kullaniciVeri);
+           
+            D1.Ayarla(true);
+
+            Console.Write(D1.yeniDizi);
+            Console.Write(D1.temelDizi);
+
+            Console.ReadLine();
+
+        }
+    }
+}
